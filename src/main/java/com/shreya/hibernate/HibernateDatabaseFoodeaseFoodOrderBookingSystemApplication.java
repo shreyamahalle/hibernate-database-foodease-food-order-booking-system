c926b402239cf0888147ad355b5a9141cc79e563
shreya mahalle <shreyamahalle2000@gmail.com>
2025-05-27 21:59:14 +0530

    hibernate project added

---
+++ src/main/java/com/shreya/hibernate/HibernateDatabaseFoodeaseFoodOrderBookingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.shreya.hibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateDatabaseFoodeaseFoodOrderBookingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateDatabaseFoodeaseFoodOrderBookingSystemApplication.class, args);
+	}
+
+}
